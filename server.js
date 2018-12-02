30ad2df8160c19cabdef8340c1ae3e2174281ce3
Björn Bung <code@bbung.de>
2018-12-02 22:19:59 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const Joi = require('joi');
+var app = express();
+
+app.use(express.json());
+
+
+var urls = [];
+
+app.get('/', (req,res) => {
+    res.send(urls);
+});
+app.get('/:url', (req, res) => {
+    
+    const result = urls.find(s => s.shortened === req.params.url);
+    if(result){
+        res.redirect(result.target);
+        return;
+    }
+    res.send(404);
+
+});
+
+const shortened = Joi.object().keys({
+    target: Joi.string().uri({
+        scheme: [
+            'http',
+            'https'
+          ]
+    }).required(),
+    shortened: Joi.string().required()
+});
+
+// create new entry
+app.post('/shorten',(req,res) => {
+    
+    const result = Joi.validate(req.body,shortened);
+    
+    if(result.error){
+        res.send(result.error);
+        return;
+    }
+    // check for existing url or target
+    console.log(req.body);
+    urls.push(req.body);
+    res.send(req.body);    
+    
+});
+
+app.listen(3000,() =>{
+    console.log('Server is up on port 3000');
+});
